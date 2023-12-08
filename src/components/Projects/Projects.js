c7bae7d5754bba26b7396b0fda950ce2c00cb6c1
Axel Alinsky <matthewkenn00@gmail.com>
2023-12-08 08:56:02 +0700

    New projects added realworldapp and realworld auto

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,8 +6,10 @@ import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.png";
 import editor from "../../Assets/Projects/codeEditor.png";
 import saucedemo from "../../Assets/Projects/saucedemo.png";
+import realworldapp from "../../Assets/Projects/realworldapp.png";
 import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
+import realworldauto from "../../Assets/Projects/realworldauto.png"
 
 function Projects() {
   return (
@@ -28,6 +30,26 @@ function Projects() {
               title="saucedemo-automation"
               description="Developed an automation testing project using Selenium, Mocha, and TypeScript, focusing on robust testing frameworks and CI/CD implementation. This project, hosted on GitHub at 'saucedemo-workspace-automation', demonstrates advanced testing strategies and continuous integration/deployment processes, aiming to enhance software quality and deployment efficiency."
               ghLink="https://github.com/AxelAlinsky/saucedemo-workspace-automation"
+              // demoLink="https://chatify-49.web.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={realworldapp}
+              isBlog={false}
+              title="real-world-app"
+              description="Created the real-world-app, a React.js website on GitHub ('realworldApp-workspace-automation'), designed for demonstrating advanced automation testing with Selenium, Mocha, and TypeScript and more. This project serves as a testbed for CI/CD practices, emphasizing software quality and efficient deployment, showcasing a practical approach to robust web application testing."
+              ghLink="https://github.com/AxelAlinsky/realworld-app"
+              // demoLink="https://chatify-49.web.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={realworldauto}
+              isBlog={false}
+              title="real-world-app"
+              description="Developed a comprehensive automation script for the real-world-app, using Mocha and TypeScript to ensure thorough testing and reliability. Hosted on GitHub, this script is part of 'realworldApp-workspace-automation', designed to rigorously test the React.js-based web application. It exemplifies best practices in automated testing, contributing significantly to the CI/CD pipeline and enhancing overall software quality."
+              ghLink="https://github.com/AxelAlinsky/realworldApp-workspace-automation"
               // demoLink="https://chatify-49.web.app/"
             />
           </Col>
